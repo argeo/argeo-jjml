c07620bd630959df70ae74746fb2a8f646492883
Mathieu Baudier <mbaudier@argeo.org>
2024-09-09 10:33:53 +0200

    Fix javac -h output directory

--- jni/generate-headers.sh
+++ jni/generate-headers.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-javac -h org_argeo_jjml_llama ../org.argeo.jjml.llama/src/org/argeo/jjml/llama/*.java
+javac -h org_argeo_jjml_llama \
+	-d ../org.argeo.jjml.llama/bin \
+	../org.argeo.jjml.llama/src/org/argeo/jjml/llama/*.java
