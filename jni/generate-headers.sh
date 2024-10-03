1195db6f5a088952320e88bf9c58e988fad5b9a0
Mathieu Baudier <mbaudier@argeo.org>
2024-10-03 15:22:44 +0200

    Single bundle for native code

--- jni/generate-headers.sh
+++ jni/generate-headers.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+/usr/lib/jvm/default-java/bin/javac \
+	-d build/classes \
+	../org.argeo.jjml.llama/src/org/argeo/jjml/ggml/*.java
 /usr/lib/jvm/default-java/bin/javac -h org_argeo_jjml_llama \
-	-d ../org.argeo.jjml.llama/bin \
+	-cp build/classes \
+	-d build/classes \
 	../org.argeo.jjml.llama/src/org/argeo/jjml/llama/*.java
