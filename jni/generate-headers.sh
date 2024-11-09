c6a68b6b1235fb8d4d5c7b5173388f869836bb7e
Mathieu Baudier <mbaudier@argeo.org>
2024-11-09 17:12:30 +0100

    Delete generated files in order to avoid problems on Windows

--- jni/generate-headers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-/usr/lib/jvm/default-java/bin/javac \
-	-d build/classes \
-	../org.argeo.jjml/src/org/argeo/jjml/ggml/*.java
-/usr/lib/jvm/default-java/bin/javac -h org_argeo_jjml_llama \
-	-cp build/classes \
-	-d build/classes \
-	../org.argeo.jjml/src/org/argeo/jjml/llama/*.java
