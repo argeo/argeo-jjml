0cacfa682532e0ff4aca05c73ce1cac3fc909e5a
Mathieu Baudier <mbaudier@argeo.org>
2025-08-30 11:47:04 +0200

    Mechanism to support older llama.cpp versions

--- native/org_argeo_jjml_llm/jjml_llm_context.cpp
+++ native/org_argeo_jjml_llm/jjml_llm_context.cpp
@@ -190,8 +190,10 @@ static void get_context_params(JNIEnv *env, jobject params,
 			env->GetMethodID(clss, "offload_kqv", "()Z"));
 	ctx_params->flash_attn = env->CallBooleanMethod(params,
 			env->GetMethodID(clss, "flash_attn", "()Z"));
+#ifndef JJML_PRE_LLAMA_0_0_5882
 	ctx_params->kv_unified = env->CallBooleanMethod(params,
 			env->GetMethodID(clss, "kv_unified", "()Z"));
+#endif
 }
 
 JNIEXPORT jobject JNICALL Java_org_argeo_jjml_llm_LlamaCppBackend_newContextParams(
@@ -225,7 +227,11 @@ JNIEXPORT jobject JNICALL Java_org_argeo_jjml_llm_LlamaCppBackend_newContextPara
 			ctx_params.no_perf, //
 			ctx_params.op_offload, //
 			ctx_params.swa_full, //
+#ifndef JJML_PRE_LLAMA_0_0_5882
 			ctx_params.kv_unified //
+#else
+			false
+#endif
 			);
 	return res;
 }
