3b720925150f235ff1c8cca5ccc078aafa4b245e
Mathieu Baudier <mbaudier@argeo.org>
2025-11-02 09:55:53 +0100

    Make model param system property name more consistent

--- org.argeo.jjml/src/org/argeo/jjml/llm/params/ModelParam.java
+++ org.argeo.jjml/src/org/argeo/jjml/llm/params/ModelParam.java
@@ -8,10 +8,13 @@ public enum ModelParam {
 	use_mlock, //
 	;
 
-	/** System property to set the default number of layers offloaded to GPU. */
-	final static String SYSTEM_PROPERTY_MODEL_PARAM_PREFIX = "jjml.llama.model.";
+	/**
+	 * System property to set model parameters, such as the default number of layers
+	 * offloaded to GPU.
+	 */
+	final static String SYSTEM_PROPERTY_MODEL_PARAM_PREFIX = "jjml.llm.model.";
 
-	/** As a system property used to override default value.*/
+	/** As a system property used to override default value. */
 	public String asSystemProperty() {
 		return SYSTEM_PROPERTY_MODEL_PARAM_PREFIX + name();
 	}
